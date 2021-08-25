698b914905573ffd132cb706aa43c01e852dab2d
Camilo <bcamilocontato@gmail.com>
2021-08-25 20:18:19 -0300

    Header concluido como fixado//   falta deixar site estilizado para telas diferentes tamanhos

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,30 +1,27 @@
 import React from 'react';
+import './style.css'
 
 export default function Header () {
   return(
     <div className="TOPO">
       <div>
-        <div>
-          <h1>Pousada <span>Secreta</span></h1>
-          <div>
-            <ul>
-              <li>
-                <a href="#">Cadastro/Login</a>
-              </li>
-              <li>
-                <a href="#">SOBRE</a>
-              </li>
-              <li>
-                <a href="#">ROTA</a>
-               </li>
-              <li>
-                <a href="#">QUARTOS</a> 
-              </li>
-            </ul>
-          </div>
-        </div>
-
+        <h1>Pousada <span> Secreta</span></h1>
       </div>
+        <ul>
+          <li>
+            <a href="#">Cadastro/Login</a>
+          </li>
+          <li>
+            <a href="#">SOBRE</a>
+          </li>
+          <li>
+            <a href="#">ROTA</a>
+            </li>
+          <li>
+            <a href="#">QUARTOS</a> 
+          </li>
+        </ul>
+      
     </div>
   );
 }
